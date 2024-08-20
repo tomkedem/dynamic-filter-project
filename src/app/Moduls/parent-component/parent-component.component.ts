b5e9881d969079df959bb30a606ac7e97c3f08d3
Tom Kedem <tompha1@hotmail.com>
2024-08-20 14:36:25 +0300

    MergedTableDynamicComponent is work

---
+++ src/app/Moduls/parent-component/parent-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent-component.component.html',
+  styleUrl: './parent-component.component.css'
+})
+export class ParentComponentComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { DynamicFormComponent } from './Moduls/dynamic-form/dynamic-form.component';
 
 import { MergedTableComponent } from './Moduls/merged-table/merged-table.component';
+import { MergedTableDynamicComponent } from './Moduls/merged-table-dynamic/merged-table-dynamic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MergedTableComponent],
+  imports: [RouterOutlet, MergedTableDynamicComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
